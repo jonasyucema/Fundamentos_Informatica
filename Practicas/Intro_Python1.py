6d9ed69ae4a2e30ea0e91cfcd5f76e57242184a4
jonasyucema <jonaasyuu@gmail.com>
2022-04-18 20:22:32 -0300

    desafios

--- Practicas/Intro_Python1.py
+++ Practicas/Intro_Python1.py
@@ -35,6 +35,37 @@ print = horas
 ##### **Ejercicio 8**
 -
 
+#####DESAFIOS
+#####DESAFIO 1
+
+Paso 9) Verificar si el mate tiene agua
+momento decisivo:
+paso 10) Si no tiene agua, buscar una pava y calentar el agua, vertirla y disfruta de tu mate
+Paso 11) si tiene agua, disfrutar del mate
+
+#####DESAFIO 2
+El prompt de ambas es el mismo. 
+El lenguaje es python.
+
+#####DESAFIO 6
+def agua_por_ronda(personas): 
+    return (personas * 30) / 1000
+
+#####DESAFIO 7
+def vaquita(costos, comensales):
+    return costos / comensales
+
+#####DESAFIO 8
+termo = 1000
+def calcular_cantidad_de_agua(personas): 
+    if termo >= (personas * 30): 
+        return personas * 30
+    else:
+        return "Necesitas mas de un termo"
+
+#####DESAFIO 9
+-
+
 
 
 
