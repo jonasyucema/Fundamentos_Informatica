435a7bb5a788004fa176a76f129c8f5a5f11d0e3
jonasyucema <jonaasyuu@gmail.com>
2022-04-18 21:28:04 -0300

    desafio

--- Practicas/Expresiones_Regulares.py
+++ Practicas/Expresiones_Regulares.py
@@ -1,6 +1,9 @@
 # **Expresiones Regulares**
 ##### **Ejercicio 1** 
-import re 
+import re
+from this import d
+
+from jmespath import search 
 cadena = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYXZ0123456789'
 
 print(re.search('\w',cadena))
@@ -22,29 +25,53 @@ string = "holaminombrees_jonas"
 patron = '\w' + '' + '\w'
 re.search(patron, string)
 
-##### **Ejercicio 5**
 
-##### **Ejercicio 6**
+#####DESAFIOS
+#####DESAFIO 1
+Podriamos usar las expresiones regulares para agilizar el 'buscar' de alguna palabra clave en algun texto y asi ahorrarnos mas tiempo-
 
-##### **Ejercicio 7**
+#####DESAFIO 2
+Inicio de línea : '^palabra'
 
-##### **Ejercicio 8**
+Inicio de texto : '\Apalabra'
 
-##### **Ejercicio 9**
+Fin de linea : 'palabra$'
 
-##### **Ejercicio 10**
+Fin de texto : 'palabra\Z'
 
-##### **Ejercicio 11**
+#####DESAFIO 3
 
-##### **Ejercicio 12**
+#####DESAFIO 4
+\d{4}
 
-##### **Ejercicio 13**
+#####DESAFIO 5
+\w [3-6]
 
-##### **Ejercicio 14**
+#####DESAFIO 6
+\. [ab]
 
-##### **Ejercicio 15**
+#####DESAFIO 7
+podriamos usar re.search(), re.findall(), re.group()
 
+#####DESAFIO 8
+\d
 
+#####DESAFIO 9
+nos buscara la aparicion de 'amet' en todo el texto
+#####DESAFIO 10
+
+#####DESAFIO 11
+la diferecia entre el re.search() y el re.match(), es que el match trata de 
+buscar la primera aparicion de la palabra, mientras que el search lo busca 
+en todo el texto
+
+#####DESAFIO 12
+La funcion .group() sirve para mostrar el resultado de una búsqueda,
+en el ejercicio de resultado nos da las veces que aparece 'amet' en el texto.
+ 
+#####DESAFIO 13
+La función sub permite reemplazar todos las ocurrencias del patrón 
+por otro patrón en un String.
 
 
 
